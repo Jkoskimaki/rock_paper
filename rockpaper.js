bbcca1ad28b31fc608c2644e4632c2ac15f21d74
Sam Tilo <tilosamantha@gmail.com>
2020-06-15 17:36:28 -0600

    organized functions

--- rockpaper.js
+++ rockpaper.js
@@ -6,14 +6,6 @@
 //   Display the user and computers choice on the screen
 //   Display the winner -->
 
-// user_hand (3 button chosen)
-// comp_hand (random choice array)
-//     if u.rock && c.scissors
-//       winner = rock
-//     elsif
-//     elsif
-//     elsif
-//     else
 
 // display the winner/tie
 // want to play again?         
@@ -24,105 +16,68 @@
 //     //   console.log(followers)
 //     //   console.log(following)
 //     // }
+
 var userInput = prompt("Choose rock, paper or scissors.")
 var compOption = ["rock", "paper", "scissors"]
-
-// alert(userInput)
-
-// officerInfo = (first, last, rank, email) =>  {
-//     //   let fullName = first + " " + last
-//     //   return({ first: first, last: last, fullName: fullName, rank: rank, email: email })
-//     // }
-
 const compAnswer = compOption[Math.floor(Math.random() * compOption.length)];
 
-function compThrow() {
-  text = "The computer chose "
-  alert(text + compAnswer);
-}
 
-compThrow(compAnswer)
+function userPlay() {
+    text = "You chose "
+    alert(text + userInput);
+  }
 
+  function compPlay() {
+    text = "The computer chose "
+    alert(text + compAnswer);
+  }
 
+  compPlay(compAnswer)
 
-var text;
+  var text;
 
-function winner() {
-    if (userInput === compAnswer) {
-        text = "You tied!"}
-    else if (userInput == "rock" && compAnswer == "scissors") {
+  function winner() {
+      if (userInput === compAnswer) {
+          text = "You tied!"}
+      else if (userInput == "rock" && compAnswer == "scissors") {
+          text = "You win!"
+      }   
+      else if (userInput == "rock" && compAnswer == "paper") {
+          text = "You lose ... paper covers rock!"   
+      }
+      else if (userInput == "paper" && compAnswer == "scissors") {
+        text = "You lose ... scissors cuts paper!";
+      }
+      else if (userInput == "paper" && compAnswer == "rock") {
         text = "You win!"
-    }   
-    else if (userInput == "rock" && compAnswer == "paper") {
-        text = "You lose ... paper covers rock!"   
-    }
-    else if (userInput == "paper" && compAnswer == "scissors") {
-      text = "You lose ... scissors cuts paper!";
-    }
-    else if (userInput == "paper" && compAnswer == "rock") {
-      text = "You win!"
-    }
-    else if (userInput == "scissors" && compAnswer == "rock") {
-        text = "You lose. The rock busts the scissors"
-    }
-    else if (userInput == "scissors" && compAnswer == "paper") {
-        text = "You win! Scissors cut paper!"
-    }
-alert(text);
-}
+      }
+      else if (userInput == "scissors" && compAnswer == "rock") {
+          text = "You lose. The rock busts the scissors"
+      }
+      else if (userInput == "scissors" && compAnswer == "paper") {
+          text = "You win! Scissors cut paper!"
+      }
+    alert(text);
+  }
 
 winner(userInput,compAnswer)
 
+var userInput2 = prompt("Would you like to play again? (yes/no)")
+
 function replay() {
-  if yes {
-    function(winner);
-  }
-  if no {
-    exit
-  }
+
+  do {
+      userPlay()
+      compPlay(compAnswer)
+      winner(userInput, compAnswer)
+  }  
+  while (userInput2 == "yes");
+  // window.userInput2
 }
 
-// if rock vs scissors 
-//   winner = rock
-// if userInput.rock vs computerInput.paper
-//   winner = computer/paper
-// if rock vs rock
-//   tie game 
-// if paper vs scissors
-//     winner = scissors
-// if paper vs rock
-//     winner = paper
-// if paper vs paper
-//     tie game
-// if scissors vs rock
-//     winner = rock
-// if scissors vs paper
-//     winner = scissors
-// if scissors vs scissors
-//     tie game
+replay(userInput, compAnswer)
 
-
-
-
-
-
-// if userInput = rock and computerInput = scissors
-//     winner = User
-
-//     var text;
-
-
-// switch(userInput) {
-//   case "Rock":
-//     text = "Excellent choice! Martini is good for your soul.";
-//     break;
-//   case "Paper":
-//     text = "Daiquiri is my favorite too!";
-//     break;
-//   case "Scissors":
-//     text = "Really? Are you sure the Cosmopolitan is your favorite?";
-//     break;
-//   default:
-//     text = "I have never heard of that one..";
-//     break;
-// 
+// do {
+//   code block to be executed
+// }
+// while (condition);
