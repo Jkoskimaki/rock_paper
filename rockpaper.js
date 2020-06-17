7b0366e036e9f8ec049161ecd6747e066dc8333a
Sam Tilo <tilosamantha@gmail.com>
2020-06-17 17:23:39 -0600

    buttons

--- rockpaper.js
+++ rockpaper.js
@@ -12,28 +12,63 @@
 // if yes/no? exit
 
 
-
-//var userChoice = document.getElementById('user-choice');
+var userChoice = document.getElementById('user-choice');
 var compChoice = document.getElementById('comp-choice');
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
 
-document.getElementById('paper').addEventListener("click", function() {
-  userChoice = "paper";
-  return userChoice;
-})
-
-function userPlay() {
-  userChoice = prompt("Choose rock, paper or scissors.")
-  text = "You chose "
-  return text + userChoice;
-}
-
+let userScore = 0;
+let compScore = 0;
 
 function compPlay() {
     compSelect = ["rock", "paper", "scissors"]
     compChoice = compSelect[Math.floor(Math.random() * compSelect.length)];
-    text = "The computer chose "
-    alert(text + compChoice);
+    // text = "The computer chose "
+    // alert(text + compChoice);
+    console.log(compChoice)
+    return compChoice
   }
+
+function userPlay() {
+  paper.addEventListener("click", function() {
+    userChoice == "paper";
+    console.log("paper")
+    //winner(compChoice + userChoice);
+  })
+
+  rock.addEventListener("click", function() {
+    userChoice == "rock";
+    console.log("rock")
+    //winner();
+  })
+
+  scissors.addEventListener("click", function() {
+    userChoice == "scissors";
+    console.log("scissors")
+    //winner();
+  })
+  return userChoice;
+}
+
+userChoice = userPlay()
+
+// userChoice = userPlay()
+
+// var response = 0;
+
+// function doSomething() {
+//     // some code
+//     return 10;
+// }
+// response = doSomething();
+
+// function userPlay() {
+//   //userChoice = prompt("Choose rock, paper or scissors.")
+//   text = "You chose "
+//   return text + userChoice;
+// }
+
 
 
   var text;
@@ -42,29 +77,35 @@ function compPlay() {
       if (userChoice === compChoice) {
           text = "You tied!"}
       else if (userChoice == "rock" && compChoice == "scissors") {
-          text = "You win!"
+        userScore += 1;
+        text = "You win!   " + userScore + ":" + compScore;
       }   
       else if (userChoice == "rock" && compChoice == "paper") {
-          text = "You lose ... paper covers rock!"   
+        compScore += 1; 
+        text = "You lose ... paper covers rock!   " + userScore + ":" + compScore;
       }
       else if (userChoice == "paper" && compChoice == "scissors") {
-        text = "You lose ... scissors cuts paper!";
+        compScore += 1;
+        text = "You lose ... scissors cuts paper!   " + userScore + ":" + compScore;
       }
       else if (userChoice == "paper" && compChoice == "rock") {
-        text = "You win!"
+        userScore += 1;
+        text = "You win!   " + userScore + ":" + compScore;
       }
       else if (userChoice == "scissors" && compChoice == "rock") {
-          text = "You lose. The rock busts the scissors"
+        compScore += 1;
+        text = "You lose. The rock busts the scissors   " + userScore + ":" + compScore;
       }
       else if (userChoice == "scissors" && compChoice == "paper") {
-          text = "You win! Scissors cut paper!"
+        userScore += 1;
+        text = "You win! Scissors cut paper!   " + userScore + ":" + compScore;
       }
-    alert(text);
+    return text;
   }
 
 
 function replay() {
-  var userInput2 = prompt("Would you like to play again? (yes/no)")
+   var userInput2 = prompt("Would you like to play again? (yes/no)")
 
   do {
     userPlay()
@@ -73,15 +114,8 @@ function replay() {
     var userInput2 = prompt("Would you like to play again? (yes/no)")
   }  
   while (userInput2 == "yes");
-  // window.userInput2
 }
 
-
-userPlay()
 compPlay()
+userPlay()
 winner()
-replay()
-
-// document.getElementById("paper").addEventListener("click", function() {
-//   myFunction(p1, p2);
-// });
